426ab43b839e0b36b27eabc86fe750cbbab8d8ae
Philip George Panicker <philipgp@outlook.com>
2024-12-08 16:38:30 +0530

    mysql

--- click-stream-processor/hello.go
+++ click-stream-processor/hello.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
 
 type trackProgress struct {
 	FingerPrint     string `json:"fingerprint"`
@@ -20,12 +26,59 @@ type trackProgress struct {
 	EventTime       string `json:"eventTime"`
 }
 
+func connectToDB() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("MY_SQL_USERNAME"),
+		Passwd: os.Getenv("MY_SQL_PASSWORD"),
+		Net:    "tcp",
+		Addr:   os.Getenv("MY_SQL_URL"), //"127.0.0.1:3306",
+		DBName: "musick",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
+func saveProgressToDB(ctx context.Context, TrackProgress trackProgress) (int64, error) {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return 0, fmt.Errorf("saveProgressToDB: %v", err)
+	}
+	defer tx.Rollback()
+	// log.Fatalf("Inserting data")
+	result, err := db.Exec("INSERT INTO track_play_progress (uid,fingerprint,progressPercent) VALUES (?, ?, ?)", TrackProgress.UID, TrackProgress.FingerPrint, TrackProgress.ProgressPercent)
+	// log.Fatalf("inserted data ")
+	if err != nil {
+		return 0, fmt.Errorf("saveProgressToDB: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("saveProgressToDB: %v", err)
+	}
+	if err = tx.Commit(); err != nil {
+		return 0, fmt.Errorf("saveProgressToDB: %v", err)
+	}
+
+	return id, nil
+}
+
 func addProgress(c *gin.Context) {
 	var trackProgress trackProgress
 	if err := c.BindJSON(&trackProgress); err != nil {
 		return
 	}
 	writeToFile(trackProgress)
+	ctx := context.TODO()
+	saveProgressToDB(ctx, trackProgress)
 	c.IndentedJSON(http.StatusCreated, trackProgress)
 }
 
@@ -45,6 +98,7 @@ func writeToFile(TrackProgress trackProgress) {
 
 func main() {
 	router := gin.Default()
+	connectToDB()
 	router.POST("/progress", addProgress)
 	router.Run(":8080")
 }
