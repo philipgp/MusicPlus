d8545b4a2dbdb9d0e4dbfa5d759f4a3eae2160af
Philip George Panicker <philipgp@outlook.com>
2024-12-16 06:13:58 +0530

    upgrade version to 2.13

--- click-stream-spark-analytics/src/main/scala/main.scala
+++ click-stream-spark-analytics/src/main/scala/main.scala
@@ -3,8 +3,8 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   //@main
   def main(args: Array[String]): Unit = {
-    val logFile = "C:\\Users\\phili\\Downloads\\spark-3.5.3-bin-hadoop3\\spark-3.5.3-bin-hadoop3\\README.md" // Should be some file on your system
-    val spark = SparkSession.builder.master("spark://192.168.1.75:7077").appName("Simple Application").getOrCreate()
+    val logFile = "C:\\Users\\phili\\Downloads\\spark-3.5.3-bin-hadoop3-scala2.13\\spark-3.5.3-bin-hadoop3-scala2.13\\README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
